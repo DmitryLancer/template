6c952e873d4b9540cb0a6dd2f23a3f8a12ed921d
DmitryLancer20 <timofeev.dooma@yandex.ru>
2022-08-21 16:57:12 +0300

    Рабочая проверка на кол во символов

--- controller/Controller.php
+++ controller/Controller.php
@@ -4,11 +4,5 @@ namespace controller;
 
 class Controller
 {
-    public function cleanParameters($value)
-    {
-        if (empty($_POST[$value])) {
-            return false;
-        }
-        return filter_var(trim($_POST[$value]), FILTER_SANITIZE_STRING);
-    }
+
 }
--- controller/RegistrationController.php
+++ controller/RegistrationController.php
@@ -4,64 +4,82 @@ if (empty($_POST)) {
     include __DIR__ . '/../view/registration.php';
 }
 
-function cleanParameters($value)
-{
-    return filter_var(trim($_POST[$value]), FILTER_SANITIZE_STRING);
-}
 
 require_once __DIR__ . '/../model/User.php';
 
 $user = new \model\User();
 
-$user->login1 = cleanParameters('login1');
-$user->password1 = cleanParameters('password1');
-$user->repeatpassword1 = cleanParameters('repeatPassword1');
-$user->age1 = cleanParameters('age1');
+$user->login1 = !empty($_POST['login1']) ? $_POST['login1'] : '';
+$user->password1 = $_POST['password1'];
+$user->repeatpassword1 = $_POST['repeatPassword1'];
+$user->age1 = $_POST['age1'];
 
-            if (!empty($_POST)) {
-                if (empty($_POST)) {
-                    echo 'Заполните логин';
-                } else {
-                    if (!$user->isPassword1Valid()) {
-                        echo "Пароль №1 содержит меньше 6 символов!";
-                        include __DIR__ . '/../view/registration.php';
-                    } else {
-                        if (!$user->isAge1Valid()) {
-                            echo 'Кому-то сюда нельзя!';
-                        }
-                    }
-                }
+if ($_POST['password1']) {
+    if (!$user->isPassword1Valid()) {
+        echo "Пароль №1 содержит меньше 6 символов!";
+    }
+}
 
-//                $login1 = !empty($_POST['login1']) ? $_POST['login1'] : '';//тернарный оператор
-                $password1 = !empty($_POST['password1']) ? $_POST['password1'] : '';
-                $repeatPassword1 = !empty($_POST['repeatPassword1']) ? $_POST['repeatPassword1'] : '';
-                $age1 = !empty($_POST['age1']) ? $_POST['age1'] : '';
+if ($user->isPassword1Valid()) {
+    $db = new PDO('mysql:host=localhost;dbname=template', 'root', 'root');
 
-                if ($repeatPassword1 != $password1) {
-                    echo "Пароли №1 не совпадают, пожалуйста, заполните форму еще раз!";
-                    include __DIR__ . '/../view/registration.php';
-                } else {
-                    echo 'Привет!';
+    // собираем данные для запроса
+    $data = [
+        ['login' => $user->login1, 'password' => $user->password1, 'age' => $user->age1],
+    ];
+    // подготавливаем SQL-запрос
+    $query = $db->prepare("INSERT INTO users (login, password, age) values (:login, :password, :age)");
+    $query->execute($data[0]);
+    if ($data) {
+        echo " Вы успешно зарегистрировались!";
+    }
+}
 
-                    try {
-                        //подключение к БД
-                        $db = new PDO('mysql:host=localhost;dbname=template', 'root', 'root');
-                    } catch (PDOException $e) {
-                        //при наличиек ошибки выводит ее
-                        print "Что-то пошло не так. Ошибка!: " . $e->getMessage() . "<br/>";//???getMessage
-                    }
-                    // собираем данные для запроса
-                    $data = [
-                        ['login' => $login1, 'password' => $password1, 'age' => $age1],
-                    ];
-                    // подготавливаем SQL-запрос
-                    $query = $db->prepare("INSERT INTO users (login, password, age) values (:login, :password, :age)");
-                    $query->execute($data[0]);
-                    if ($data) {
-                        echo " Вы успешно зарегистрировались!";
-                    }
-                }
-            }
+
+//            if (!empty($_POST)) {
+//                if (empty($_POST)) {
+//                    echo 'Заполните логин';
+//                } else {
+//                    if (!$user->isPassword1Valid()) {
+//                        echo "Пароль №1 содержит меньше 6 символов!";
+//                        include __DIR__ . '/../view/registration.php';
+//                    } else {
+//                        if (!$user->isAge1Valid()) {
+//                            echo 'Кому-то сюда нельзя!';
+//                        }
+//                    }
+//                }
+//
+////                $login1 = !empty($_POST['login1']) ? $_POST['login1'] : '';//тернарный оператор
+//                $password1 = !empty($_POST['password1']) ? $_POST['password1'] : '';
+//                $repeatPassword1 = !empty($_POST['repeatPassword1']) ? $_POST['repeatPassword1'] : '';
+//                $age1 = !empty($_POST['age1']) ? $_POST['age1'] : '';
+//
+//                if ($repeatPassword1 != $password1) {
+//                    echo "Пароли №1 не совпадают, пожалуйста, заполните форму еще раз!";
+//                    include __DIR__ . '/../view/registration.php';
+//                } else {
+//                    echo 'Привет!';
+//
+//                    try {
+//                        //подключение к БД
+//                        $db = new PDO('mysql:host=localhost;dbname=template', 'root', 'root');
+//                    } catch (PDOException $e) {
+//                        //при наличиек ошибки выводит ее
+//                        print "Что-то пошло не так. Ошибка!: " . $e->getMessage() . "<br/>";//???getMessage
+//                    }
+//                    // собираем данные для запроса
+//                    $data = [
+//                        ['login' => $user->login1, 'password' => $password1, 'age' => $age1],
+//                    ];
+//                    // подготавливаем SQL-запрос
+//                    $query = $db->prepare("INSERT INTO users (login, password, age) values (:login, :password, :age)");
+//                    $query->execute($data[0]);
+//                    if ($data) {
+//                        echo " Вы успешно зарегистрировались!";
+//                    }
+//                }
+//            }
         
 
 
