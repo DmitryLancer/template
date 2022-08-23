aa5a0909d8e2b0f439435097b591ef341c8c95f3
DmitryLancer20 <timofeev.dooma@yandex.ru>
2022-08-23 20:49:13 +0300

    валиадация пароля работает без ошибок

--- controller/RegistrationController.php
+++ controller/RegistrationController.php
@@ -9,24 +9,26 @@ require_once __DIR__ . '/../model/User.php';
 
 $user = new \model\User();
 
-
-
-
-
 $user->login1 = !empty($_POST['login1']) ? $_POST['login1'] : '';
+$user->age1 = $_POST['age1'];
 //$user->repeatPassword1 = !empty($_POST['repeatPassword1']) ? $_POST['repeatPassword1'] : '';
 
-if (!empty($_POST['password1'])) {
+if (!empty($_POST['password1'])) { /// должен быть password1 или login1? ???
     $user->password1 = $_POST['password1'];
 } else {
     $user->password1 = '';
 }
-//$user->repeatpassword1 = $_POST['repeatPassword1'];
-$user->age1 = $_POST['age1'];
 
-if (!$user->isPassword1Valid()) {
-    echo "Пароль №1 содержит меньше 6 символов!";
-} else {
+if (!empty($_POST)) {
+    if (!$user->isPassword1Valid()) {
+        echo "Пароль №1 содержит меньше 6 символов!";
+    }
+}
+
+
+//$user->repeatpassword1 = $_POST['repeatPassword1'];
+
+if ($user->isPassword1Valid()) {
     $db = new PDO('mysql:host=localhost;dbname=template', 'root', 'root');
 
     // собираем данные для запроса
@@ -40,6 +42,37 @@ if (!$user->isPassword1Valid()) {
         echo " Вы успешно зарегистрировались!";
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//if (!$user->isPassword1Valid()) {
+//    echo "Пароль №1 содержит меньше 6 символов!";
+//} else {
+//    $db = new PDO('mysql:host=localhost;dbname=template', 'root', 'root');
+//
+//    // собираем данные для запроса
+//    $data = [
+//        ['login' => $user->login1, 'password' => $user->password1, 'age' => $user->age1],
+//    ];
+//    // подготавливаем SQL-запрос
+//    $query = $db->prepare("INSERT INTO users (login, password, age) values (:login, :password, :age)");
+//    $query->execute($data[0]);
+//    if ($data) {
+//        echo " Вы успешно зарегистрировались!";
+//    }
+//}
 
 
 //if ($repeatPassword1 != $password1) {
