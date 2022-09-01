0f009e81a5b0d8e15af6d1e604be342fd3b17e38
DmitryLancer20 <timofeev.dooma@yandex.ru>
2022-09-01 20:21:05 +0300

    сделал сохранение в БД

--- index.php
+++ index.php
@@ -7,39 +7,8 @@
 //});
 
 
-//if(empty($_POST)) {
-//    include __DIR__ . 'controller/RegistrationController.php';
-//    $RegistrationController = new \controller\RegistrationController();
-//    $RegistrationController->actionIndex();
-//
-//}
 
-//include_once __DIR__ . '/controller/RegistrationController.php';
-
-//if(empty($_POST)) {
-//    require __DIR__ . '/controller/RegistrationController.php';
-//    $RegistrationController = new \controller\RegistrationController();
-//    $RegistrationController->actionRegistration();
-//
-//}
-
-
-//if (!empty($_POST['action'])) {
-//    if ($_POST['action'] == 'post') {
-//        include_once __DIR__ . '/controller/PostController.php';
-//        $postController = new \controller\PostController();
-//        $postController->actionIndex();
-//    }
-//}
-
-
-//$url = $_SERVER['REQUEST_URI'];
-
-spl_autoload_register(function ($class_name) {
-    include $class_name . '.php';
-});
-
-
+$url = $_SERVER['REQUEST_URI'];
 
 switch ($url) {
     case '/':
